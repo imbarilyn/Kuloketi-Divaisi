568d4bec7793071040d65aabcf14decd1056a26d
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-09-03 12:41:35 +0300

    added comments on the index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// let popup = document.querySelector('#popup');
-// function openPopup (){
-//     popup.classList.add("open-popup");
-
-// }
-
-// function closePopup (){
-//     popup.classList.remove("open-popup");
-// }
+//The openPopup function executes once one the user clicks the Locate Me button
 function openPopup(){
     let popup = document.querySelector('#popup');
     popup.classList.add("open-popup");
     let locate = document.querySelector('p');
+// The success executes once the user allows the browser to get location
     function success (position){
         const location = {lat:position.coords.latitude, lng:position.coords.longitude};
         console.log(location.lat+''+ location.lng);
@@ -21,18 +14,20 @@ function openPopup(){
             locate.textContent = loc.principalSubdivision;
         })   
     }
+// error function executes once there is an error in getting the geolaction object
 
     function error (error){
         alert(`${error.mesage}`);
 
     }
-
+// The options object specifies more on fresh time accuracy and time span for cached information
     const options = {
         timeOut: 15000,
         maximumAge: 40000,
         enableHighAccuracy: 350
 
     }
+//the if function checks if the browser has the geolacation functionality
     if(!navigator.geolocation){
         alert("Oppss!! seems there is no gelocation functionality");
     }
@@ -40,6 +35,7 @@ function openPopup(){
         navigator.geolocation.getCurrentPosition(success, error);
     }
 }
+// closePopup function closes the pop up when the user clicks the Close button
 function closePopup (){
     popup.classList.remove("open-popup");
 }
